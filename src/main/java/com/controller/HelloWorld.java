eced0bca7fd47691f031390b292899871499ff70
kirti <sawantkriti4@gmail.com>
2021-06-17 13:07:10 +0530

    Initial commit1

---
+++ src/main/java/com/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping(value = "/home")
+    String Home(){
+        return "From Home Page...!! ";
+    }
+    @RequestMapping(value = "/about")
+    String About(){
+        return "Welcome SpringBoot...!! ";
+    }
+}
